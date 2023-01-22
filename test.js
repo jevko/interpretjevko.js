38e0c4bfb6bba837e9dc10f092cf7e28d7c7d2d9
Darius J Chuck <darius.j.chuck+github@gmail.com>
2023-01-22 23:02:19 +0100

    add test for parsing a complex object

--- test.js
+++ test.js
@@ -13,9 +13,92 @@ Deno.test('basic', () => {
         [x][y][z]
       ]
     ]
+    person [
+      first name [John]
+      last name [Smith]
+      is alive [true]
+      age [27]
+      address [
+        street address [21 2nd Street]
+        city [New York]
+        state [NY]
+        postal code [10021-3100]
+      ]
+      phone numbers [
+        [
+          type [home]
+          number [212 555-1234]
+        ]
+        [
+          type [office]
+          number [646 555-4567]
+        ]
+      ]
+      children [seq]
+      spouse [nil]
+    ]
   `)
 
+  const parsePhoneNumber = jevko => {
+    let _ = mod.toMap(jevko)
+    return {
+      type: mod.toString(_.type),
+      number: mod.toString(_.number),
+    }
+  }
+
+  const parseSpouse = jevko => {
+    if (jevko.subjevkos.length === 0) {
+      if (jevko.suffix === 'nil') return null
+    }
+    throw Error('not implemented')
+  }
+
+  const parsePerson = jevko => {
+    let _ = mod.toMap(jevko)
+    let address
+    {
+      let a = mod.toMap(_.address)
+      address = {
+        streetAddress: mod.toString(a['street address']),
+        city: mod.toString(a.city),
+        state: mod.toString(a.state),
+        postalCode: mod.toString(a['postal code']),
+      }
+    }
+    return {
+      firstName: mod.toString(_['first name']),
+      lastName: mod.toString(_['last name']),
+      isAlive: mod.toBoolean(_['is alive']),
+      age: mod.toNumber(_.age),
+      address,
+      phoneNumbers: mod.toList(_['phone numbers']).map(_ => parsePhoneNumber(_)),
+      children: mod.toList(_.children),
+      spouse: parseSpouse(_.spouse),
+    }
+  }
+
   let _ = mod.toMap(jevko)
+
+  assertEquals(parsePerson(_.person), {
+    firstName: "John",
+    lastName: "Smith",
+    isAlive: true,
+    age: 27,
+    address: {
+      streetAddress: "21 2nd Street",
+      city: "New York",
+      state: "NY",
+      postalCode: "10021-3100"
+    },
+    phoneNumbers: [
+      { type: "home", number: "212 555-1234" },
+      { type: "office", number: "646 555-4567" }
+    ],
+    children: [],
+    spouse: null
+  })
+
   _ = mod.toList(_.list)
 
   const str = mod.toString(_[2])
